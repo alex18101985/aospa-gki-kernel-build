04a0d4d4f078fb4d7411fc81035a795d80b01e7a
alex18101985 <alex18101985@gmail.com>
2026-02-24 16:19:09 +0300

    Update build.sh

--- android_kernel_xiaomi_sm8450/build.sh
+++ android_kernel_xiaomi_sm8450/build.sh
@@ -144,7 +144,7 @@ echo -e "Generating config...\n"
 m $DEFCONFIG
 
 # Merge vendor configs
-./scripts/kconfig/merge_config.sh -O out out/.config $DEFCONFIGS vendor/${TARGET}_GKI.config
+m ./scripts/kconfig/merge_config.sh $DEFCONFIGS vendor/${TARGET}_GKI.config
 
 # Apply version
 scripts/config --file out/.config \
