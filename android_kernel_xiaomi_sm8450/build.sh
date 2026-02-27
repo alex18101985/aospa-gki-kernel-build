fee3cebba3ea0e6fd7c054aa4a9fd1038b2ebdf8
alex18101985 <alex18101985@gmail.com>
2026-02-27 19:30:38 +0300

    Update build.sh

--- android_kernel_xiaomi_sm8450/build.sh
+++ android_kernel_xiaomi_sm8450/build.sh
@@ -98,6 +98,7 @@ export PATH="$TC_DIR/bin:$PREBUILTS_DIR/bin:$PATH"
 function m() {
     make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 \
         KBUILD_BUILD_USER=alex KBUILD_BUILD_HOST=github-build \
+        LDFLAGS="-Wl,--threads --thinlto-jobs=$(nproc --all)" \
         DTC_EXT="$PREBUILTS_DIR/bin/dtc" \
         DTC_OVERLAY_TEST_EXT="$PREBUILTS_DIR/bin/ufdt_apply_overlay" \
         TARGET_PRODUCT=$TARGET $@ || exit $?
